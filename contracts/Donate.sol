90909f62c3bbb0dd73d4aa7239e48997549ca6d5
HiZhongxh <zhong_xiaohong@yeah.net>
2018-12-30 17:50:24 +0800

    2 contracts had tested ok in remix

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -1,11 +1,55 @@
 pragma solidity ^0.4.14;
 
-import './Ownable.sol';
+// import './Ownable.sol';
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+}
 
 contract Donate is Ownable {
 
     event AddFund(address indexed from, uint value);
     event ExtractFund(address indexed from, uint value);
+
+    function Donate() payable public {
+        
+    }
 
     function addFund() payable public returns (uint) {
         AddFund(msg.sender, msg.value);
@@ -13,7 +57,7 @@ contract Donate is Ownable {
     }
 
     function extractFund() onlyOwner {
-        ExtractFund(msg.sender, address(this).balance)
+        ExtractFund(msg.sender, address(this).balance);
         selfdestruct(owner);
     }
 }
--- contracts/manage.sol
+++ contracts/manage.sol
@@ -1,17 +1,70 @@
 pragma solidity ^0.4.14;
 
-import './Ownable.sol';
+// import './Ownable.sol';
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+}
 
 contract Manage is Ownable {
 
     event AddDonor(address indexed from, address donorAddr, address contractAddr);
     event Resolve(address indexed from, uint value);
 
-    mapping(address => address) public contracts;
+    struct DonateInfo {
+        address contractAddr;
+        string materialsAddr;
+    }
 
-    function addDonor(address donorAddr, address contractAddr) onlyOwner {
+    mapping(address => DonateInfo) public contracts;
+    
+    function Manage() payable public {
+        
+    }
+
+    function addDonor(address donorAddr, address contractAddr, string materialsAddr) onlyOwner {
         AddDonor(msg.sender, donorAddr, contractAddr);
-        contracts[donorAddr] = contractAddr;
+        contracts[donorAddr] = DonateInfo(contractAddr, materialsAddr);
+    }
+
+    function getDonor(address donorAddr) returns (address contractAddr, string materialsAddr) {
+        contractAddr = contracts[donorAddr].contractAddr;
+        materialsAddr = contracts[donorAddr].materialsAddr;
     }
 
     function resolve() onlyOwner {
